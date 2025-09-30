95cf09c0971ecc85a96bfcd5c7ea7d55eb7cce3a
GustavoSJ777 <gustavo.bcastiglioni@aluno.edu.es.gov.br>
2025-09-30 11:07:44 -0300

    Create index.php

---
+++ aula06/index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+    </head>
+        <title>Aula06 </title>
+    <body>
+        <h3>Operadores de Atribuição</h3>
+        <hr>
+
+        <?php
+        $a=2;
+        $b=$a=5;
+        Echo "A e igual a $a e B e igual a $b.";
+        echo "<br></br>";
+
+        $a=$a+8;
+        echo "Agora A e igual $a";
+        echo "<br></br>";
+
+        $b-=5;
+        echo "Agora B e igual a $b.";
+        echo "<br></br>";
+
+
+        ?>
+    </body>
+</html>
