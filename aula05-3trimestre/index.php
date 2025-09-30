90a87c77fed781914c506b6c21291f83d5b3a8d4
GustavoSJ777 <gustavo.bcastiglioni@aluno.edu.es.gov.br>
2025-09-30 10:45:14 -0300

    Update index.php

--- aula05-3trimestre/index.php
+++ aula05-3trimestre/index.php
@@ -8,13 +8,14 @@
             $n2= $_GET["x"];
 
             echo "Os valores informados são $n1 e $n2 <br>";
-            echo "O valor absoluto de $n2 e". abs($n2);
+            echo "<br> O valor absoluto de $n2 e". abs($n2);
             echo "<br> O resultado de  $n2 <sup>$n1</sup> e igual a ".pow($n2,$n1);
-            echo "A raiz quadrada de $n2 e ".sqrt($n2);
+            echo "<br> A raiz quadrada de $n2 e ".sqrt($n2);
             echo "<br> O numero $n2 arredondado e ".round($n2);
             echo "<br> O numero $n2 arredondado para cima".ceil($n2);
             echo "<br> O numero $n2 arredondado para baixo".floor($n2);
-            echo "<br>"        
+            echo "<br> A parte inteira de $n2 e igual a ". intval($n2);
+            echo "<br> Um pirulito custa R$".number_format($n2,2);        
             ?>
     </body>
 </html>
