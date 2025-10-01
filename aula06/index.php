a37e080a88c7a050fa8dd4fb120181202a69af24
GustavoSJ777 <gustavo.bcastiglioni@aluno.edu.es.gov.br>
2025-10-01 10:58:30 -0300

    Update index.php

--- aula06/index.php
+++ aula06/index.php
@@ -8,7 +8,7 @@
 
         <?php
         $a=2;
-        $b=$a=5;
+        $b=$a+5;
         Echo "A e igual a $a e B e igual a $b.";
         echo "<br></br>";
 
@@ -24,6 +24,21 @@
         echo "Agora B e igual a $b.";
         echo "<br></br>";
 
+        $b*=2;
+        echo "Agora Multiplicando o valor de B por 2 e igual a $b.";
+        echo "<br></br>";
+
+        $b/=10;
+        echo "Agora Dividindo o valor de B por 10 e igual a $b.";
+        echo "<br></br>";
+
+        $b%=3;
+        echo "O resto da divisão do valor de B dividido por 3 e igual a $b.";
+        echo "<br></br>";
+
+        $b.=10;
+        echo "Concatenando o valor de B com o numero 10 fica igual a $b.";
+        echo "<br></br>";
         ?>
     </body>
 </html>
