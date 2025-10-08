93c532cadc9387049ce3f0a801f62639833af078
GustavoSJ777 <gustavo.bcastiglioni@aluno.edu.es.gov.br>
2025-10-08 10:40:38 -0300

    Update index.php

--- aula07/index.php
+++ aula07/index.php
@@ -10,28 +10,22 @@
 
             $num=10;
             echo"Valor inicial : $num";
-            <br>
             
             $num+=5;
             echo"<br><br>A soma de 10 mais 5 e : $num";
-            <br>
 
             $num-=2;
             echo"<br>Apos subtrair 2 : $num";
-            <br>
 
             $num*=3;
             echo"<br>Apos multiplica por 3 : $num";
-            <br>
 
             $num/=2;
             echo"<br>Apos dividi por 2 : $num";
-            <br>
 
             ++$num;
             echo"<br><br>Apos Pré-incremento $num";
             echo"<br>Apos Pós-incremento : " .($num++);
-            <br>
 
             --$num;
             echo"<br>Apos Pré-encremento $num";
